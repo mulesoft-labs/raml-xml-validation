473d7350765e7ab6385e25123a51930c0262d7ab
dreamflyer <gleb@onpositive.com>
2017-01-09 23:45:33 +0600

    api update.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ declare function require(arg: string): any;
 
 var validator = require('xmllint-jsparser');
 
+var fs = require('fs');
+
 export class XMLValidator {
     private schemaObject: any;
 
@@ -9,9 +11,31 @@ export class XMLValidator {
 
     }
 
-    validate(xml: string): Error[] {
-        var result = validator.validateXML({xml: xml, schema: this.schema});
+    validate(xml: string, references: XMLSchemaReference[] = []): Error[] {
+        if(references.length === 0) {
+            var result = validator.validateXML({xml: xml, schema: this.schema});
+            
+            return (result && result.errors && result.errors.map((error: any) => new Error(error))) || [];
+        }
+        
+        var schemas: string[] = this.remoteSchemas(references);
 
-        return (result && result.errors && result.errors.map((error: any) => new Error(error))) || [];
+        schemas.push(this.schema);
+        
+        var result = validator.validateXML({xml: xml, schema: schemas});
+
+        return (result && result.errors && result.errors.map((error: any) => {
+                return new Error(error)
+            })) || [];
+    }
+    
+    private remoteSchemas(refs: XMLSchemaReference[]): string[] {
+        return refs.map(ref => ref.patchedContent);
+    }
+}
+
+export class XMLSchemaReference {
+    constructor(public originalPath: string, public virtualIndex: number, public patchedContent: string) {
+        
     }
 }
